da1cb79f440a4cdadc8fb1e6bd62e8d7ce0891c7
Jim <jim@jam.sessionsnet.org>
2021-10-28 19:28:13 -0700

    set background color when initializing game

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
