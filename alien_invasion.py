111f44481d976f5e09eca3dfbacbfec3a04c3684
Jim <jim@jam.sessionsnet.org>
2021-10-28 21:30:13 -0700

    make new Settings object to put in AlienInvasion object when it's initialized

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game and create game resources."""
         pygame.init()
+        self.settings = Settings()
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
