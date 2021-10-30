0fd246d7bd77fc7931a86e03742e5f4ccff7b1a8
Jim <jim@jam.sessionsnet.org>
2021-10-30 12:10:08 -0700

    fix typo involving its y attribute

--- bullet.py
+++ bullet.py
@@ -9,14 +9,14 @@ class Bullet(Sprite):
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        self.color = self.settings.buillet_color
+        self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0, 0) and then set correct position.
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
         # Stire the bullet's position as a decimal value.
-        self.y - float(self.rect.y)
+        self.y = float(self.rect.y)
 
     def update(self):
         """Move the bullet up the screen."""
