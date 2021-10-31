327c9f09fb7a1ec5e1edb552877b31538c06bccf
Jim <jim@jam.sessionsnet.org>
2021-10-31 09:57:24 -0700

    REFACTOR: add row_number parameter to _create_alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -92,12 +92,13 @@ class AlienInvasion:
         for alien_number in range(number_aliens_x):
             self._create_alien(alien_number)
 
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
         """Create an alien and place it in the row."""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
