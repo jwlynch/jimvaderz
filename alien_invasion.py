f41bb1208205292d3ac43001bb3a0f42918a6a91
Jim <jim@jam.sessionsnet.org>
2021-10-31 10:07:34 -0700

    determine how many rows of aliens we can have on the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,6 +88,11 @@ class AlienInvasion:
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
+        # Determine the number of rows of aliens that fit on the screen.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                              (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
 
     def _create_alien(self, alien_number, row_number):
         """Create an alien and place it in the row."""
