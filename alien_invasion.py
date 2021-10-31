8434ca0ea57a81ee855c9231957bc6dbeb98ba06
Jim <jim@jam.sessionsnet.org>
2021-10-30 22:15:07 -0700

    import Alien class so we can work with aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from  settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
