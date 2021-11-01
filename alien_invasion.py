442f3e2eeba9ee5454d1dc0fc22f8de668c1ef02
Jim <jim@jam.sessionsnet.org>
2021-10-31 19:37:29 -0700

    add AlienInvasion::_change_fleet_direction() to move fleet down, and reverse its left-right direction

--- alien_invasion.py
+++ alien_invasion.py
@@ -113,6 +113,13 @@ class AlienInvasion:
         alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
